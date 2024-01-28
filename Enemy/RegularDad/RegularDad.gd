92c7e312db8f812eca4f9041c971af6977a11a09
HypotheticalWhale <samueltan.9803@gmail.com>
2024-01-28 10:05:03 +0800

    collision stuff

--- Enemy/RegularDad/RegularDad.gd
+++ Enemy/RegularDad/RegularDad.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 @export var direction: Vector2 = Vector2.LEFT  # Adjust the direction as needed
 @export var spawn_interval: float = 2.0
 @export var damage: int = 1
+@export var exp:int = 1
 
 func _ready():
 	velocity = direction * speed
@@ -15,7 +16,11 @@ func _process(delta: float) -> void:
 		await get_tree().create_timer(2.0).timeout
 		self.queue_free()
 
-
 func _on_hurtbox_body_entered(body):
-	
+	self.queue_free()
+
+
+func _on_hurtbox_area_entered(area):	
+	print(area.owner.get_parent())
+	GlobalVars.current_exp += exp
 	self.queue_free()
