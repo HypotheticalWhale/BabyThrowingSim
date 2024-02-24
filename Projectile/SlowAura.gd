d5a58a87ff870f0f98cf7aebc422a293dd5442a3
Yew Chian's Dell <mykatsudon@gmail.com>
2024-02-24 10:34:35 +0800

    scene change from interior to combat and sprites for slow and freeze

--- Projectile/SlowAura.gd
+++ Projectile/SlowAura.gd
@@ -3,6 +3,7 @@ var type = "slow_aura"
 var slow_multiplier = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$AnimationPlayer.play("wow_trippy")
 	pass # Replace with function body.
 
 
--- UI/SceneManager.gd
+++ UI/SceneManager.gd
@@ -10,9 +10,9 @@ func change_scene(old_scene, new_scene):
 		await trapdoor_tween.tween_property(trapdoor, "rotation_degrees", -125, 0.5).finished
 		
 		var player = get_tree().current_scene.get_node("Player")
-		player.get_node("AnimationPlayer").play("to_trapdoor")
+		player.get_node("AnimationPlayer").play("combat_to_trapdoor")
 		await player.get_node("AnimationPlayer").animation_finished
-		player.get_node("AnimationPlayer").play("to_interior")
+		player.get_node("AnimationPlayer").play("trapdoor_to_interior")
 		await player.get_node("AnimationPlayer").animation_finished
 		
 		var camera_movement_tween = get_tree().create_tween()
@@ -34,4 +34,40 @@ func change_scene(old_scene, new_scene):
 			porch_wall_final_modulate,
 			camera_movement_duration
 		)
+		
+		get_tree().current_scene.get_node("Porch/StartButton").visible = true
+	
+	if old_scene == "interior" and new_scene == "combat":
+		get_tree().current_scene.get_node("Porch/StartButton").visible = false
+		
+		var player = get_tree().current_scene.get_node("Player")
+		player.get_node("AnimationPlayer").play("interior_to_trapdoor")
+		await player.get_node("AnimationPlayer").animation_finished
+		player.get_node("AnimationPlayer").play("trapdoor_to_combat")
+		await player.get_node("AnimationPlayer").animation_finished
+		
+		var trapdoor_tween = get_tree().create_tween()
+		var trapdoor = get_tree().current_scene.get_node("Porch/Trapdoor")
+		await trapdoor_tween.tween_property(trapdoor, "rotation_degrees", 0, 0.5).finished
+		
+		var camera_movement_tween = get_tree().create_tween()
+		var new_camera_position = get_tree().current_scene.get_node("CombatCameraMarker").global_position
+		var camera_movement_duration = 1
+		camera_movement_tween.tween_property(camera, "global_position", new_camera_position, camera_movement_duration).set_trans(Tween.TRANS_QUAD)
+		
+		var camera_zoom_tween = get_tree().create_tween()
+		var new_camera_zoom = Vector2(1, 1)
+		camera_zoom_tween.tween_property(camera, "zoom", new_camera_zoom, camera_movement_duration)
+		
+		var porch_wall_transparency_tween = get_tree().create_tween()
+		var porch_wall = get_tree().current_scene.get_node("Porch/PorchWall")
+		var porch_wall_final_modulate = Color(1, 1, 1, 1)
+		
+		porch_wall_transparency_tween.tween_property(
+			porch_wall,
+			"modulate",
+			porch_wall_final_modulate,
+			camera_movement_duration
+		)
+		
 
