53d64fdc6a177bbc6ad166cfc49ffb5dd35f6e40
HypotheticalWhale <samueltan.9803@gmail.com>
2024-04-22 13:35:48 +0800

    added a new billboard ad :)

--- Scenes/Billboard.gd
+++ Scenes/Billboard.gd
@@ -1,7 +1,8 @@
 extends Sprite2D
 @onready var texture1 = preload("res://assets/BillboardContent.png")
 @onready var texture2 = preload("res://assets/BillboardContent2.png")
-var all_textures = [texture1,texture2]
+@onready var texture3 = preload("res://assets/BillboardContent3.png")
+@onready var all_textures = [texture1,texture2,texture3]
 var current_index = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,13 +14,12 @@ func _process(delta):
 	pass
 	
 func get_next_item():
-	print(all_textures[current_index])
 	current_index += 1
 	if current_index >= all_textures.size():
 		current_index = 0
-
+	print(all_textures)
+	return all_textures[current_index]
+	
 func _on_change_content_timer_timeout():
-	if texture == texture1:
-		texture = texture2
-	else:
-		texture = texture1
+	print(get_next_item())
+	texture = get_next_item()
