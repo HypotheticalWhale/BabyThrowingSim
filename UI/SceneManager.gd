a80cbfbe6527b79accbbe4fb70b734b8cc60b5bb
HypotheticalWhale <samueltan.9803@gmail.com>
2024-04-24 14:52:33 +0800

    oh i forgot i was fixing a bug, now it is fixed

--- UI/SceneManager.gd
+++ UI/SceneManager.gd
@@ -9,25 +9,26 @@ func change_scene(old_scene, new_scene):
 		var trapdoor = get_tree().current_scene.get_node("Porch/Trapdoor")
 		await trapdoor_tween.tween_property(trapdoor, "rotation_degrees", -125, 0.5).finished
 		
-		#var player = get_tree().current_scene.get_node("Player")
-		#player.get_node("AnimationPlayer").play("combat_to_trapdoor")
-		#await player.get_node("AnimationPlayer").animation_finished
-		#player.get_node("AnimationPlayer").play("trapdoor_to_interior")
-		#await player.get_node("AnimationPlayer").animation_finished
-		
-		var camera_movement_tween = get_tree().create_tween()
 		var new_camera_position = get_tree().current_scene.get_node("InteriorCameraMarker").global_position
 		var player_position = get_tree().current_scene.get_node("Player").global_position
 		var camera_movement_duration = 1
 		var new_camera_zoom = Vector2(6, 6)
 		var camera_zoom_tween = get_tree().create_tween()		
+		var camera_movement_tween = get_tree().create_tween()		
 		camera_movement_tween.tween_property(camera, "global_position", player_position, camera_movement_duration).set_trans(Tween.TRANS_QUAD)		
 		Engine.time_scale = 0.5				
 		await camera_zoom_tween.tween_property(camera, "zoom", new_camera_zoom, camera_movement_duration).finished
+		camera_movement_tween = get_tree().create_tween()		
 		camera_movement_tween.tween_property(camera, "global_position", new_camera_position, camera_movement_duration).set_trans(Tween.TRANS_QUAD)
 		new_camera_zoom = Vector2(4, 4)
 		camera_zoom_tween.tween_property(camera, "zoom", new_camera_zoom, camera_movement_duration)
 		Engine.time_scale = 1		
+		await get_tree().create_timer(1)
+		var player = get_tree().current_scene.get_node("Player")
+		player.get_node("AnimationPlayer").play("combat_to_trapdoor")
+		await player.get_node("AnimationPlayer").animation_finished
+		player.get_node("AnimationPlayer").play("trapdoor_to_interior")
+		await player.get_node("AnimationPlayer").animation_finished
 		var porch_wall_transparency_tween = get_tree().create_tween()
 		var porch_wall = get_tree().current_scene.get_node("Porch/PorchWall")
 		var porch_wall_final_modulate = Color(1, 1, 1, 0.2)
