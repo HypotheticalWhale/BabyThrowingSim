26252bdddc196ae1f5b9e67baaf67d2f27b86cdc
HypotheticalWhale <samueltan.9803@gmail.com>
2024-04-27 11:02:57 +0800

    life is so hard man why doesnt it just work

--- UI/StartButton.gd
+++ UI/StartButton.gd
@@ -4,9 +4,10 @@ extends TextureButton
 
 
 func _on_pressed():
+	disabled = true
 	await SceneManager.change_scene("interior", "combat")
+	GlobalVars.can_click = true
 	get_tree().current_scene.start_all_timers()
-	
 	GlobalVars.process_mode = Node.PROCESS_MODE_ALWAYS
 	get_tree().current_scene.get_node("Player").MAX_HEALTH = 1
 	GlobalVars.current_health = 1
